d970385ec70814f052e4105b969d66b3dbcabe40
Steve Genoud <steve.genoud@nagra.com>
2019-02-26 15:44:59 +0100

    Add a hook for outside click handling

--- README.md
+++ README.md
@@ -11,5 +11,14 @@ welcome.
 
 ##### Helper to create tooltips
 
-`hooks/usePopper.js`: glue code between popper.js and React
-`components/Tooltip.jsx`: the base styled components for building tooltips
+- `hooks/usePopper.js`: glue code between popper.js and React. Read more about
+  is [here](https://carrots.sgenoud.com/use-popper.js-in-react/)
+
+- `components/Tooltip.jsx`: the base styled components for building tooltips
+
+##### Hooks for simple, well defined, tasks
+
+- `hooks/useOutsideCallback.jsx`: hooks to defined a callback that will be
+  called when a user clicks outside a list of targets. What is different about
+  this implementation is that you can define an array of nodes that are
+  considered the "inside".
---
+++ hooks/useOutsideCallback.js
@@ -0,0 +1,57 @@
+/* global document */
+import { useRef, useEffect } from 'react';
+
+const EVENTS = ['click', 'touchstart'];
+
+// Calls the handler when one of the events is triggered outside of the list of
+// input nodes.
+// Note that the events list is not refreshed after the initialisation
+//
+// This code owes a lot to outy https://github.com/souporserious/outy
+export default (inputNodes, handler, events = EVENTS) => {
+  const handlerRef = useRef(handler);
+  let nodes = Array.isArray(inputNodes) ? inputNodes : [inputNodes];
+  if (!inputNodes) {
+    nodes = null;
+  }
+  const nodesRef = useRef(nodes);
+
+  useEffect(() => {
+    handlerRef.current = handler;
+    nodesRef.current = nodes;
+  });
+
+  useEffect(
+    () => {
+      if (!handler || !nodes) {
+        return;
+      }
+
+      const listener = event => {
+        const isEventInside = node => {
+          if (!node || !node.current || !handlerRef.current) {
+            return false;
+          }
+          return node.current.contains(event.target);
+        };
+
+        // The event needs to be outside all the nodes to call the handler
+        if (!nodesRef.current.some(isEventInside)) {
+          handlerRef.current(event);
+        }
+      };
+
+      events.forEach(event => {
+        document.addEventListener(event, listener);
+      });
+
+      // eslint-disable-next-line consistent-return
+      return () => {
+        events.forEach(event => {
+          document.removeEventListener(event, listener);
+        });
+      };
+    },
+    [!handler, !nodes]
+  );
+};
